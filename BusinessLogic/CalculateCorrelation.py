c725187ad2db34e262ea9394165c74f3f651d385
Preetam <pritambanerjee40@gmail.com>
2018-01-21 17:15:01 +0530

    Making progress on Correlation calculation

--- BusinessLogic/CalculateCorrelation.py
+++ BusinessLogic/CalculateCorrelation.py
@@ -1,5 +1,9 @@
 from DataAccess.CassandraConn import session
 from collections import OrderedDict
+import numpy as np
+
+window_size = 500
+decay_lambda = 0.98
 
 
 class CalculateHistoricalCorrelation:
@@ -9,7 +13,18 @@ class CalculateHistoricalCorrelation:
 
     def calculate_return_and_entropy(self):
         eod_dates, eod_spots1, eod_spots2 = self.compare_dates_and_clean()
-        print eod_dates, eod_spots1, eod_spots2
+        return_s1, return_s2 = [], []
+        for idx in xrange(1, len(eod_dates), 1):
+            return_s1.append(eod_spots1[idx] / eod_spots1[idx - 1] - 1)
+            return_s2.append(eod_spots2[idx] / eod_spots2[idx - 1] - 1)
+        decay_factors = self.get_entropy()
+        hist_corr, dates_for_corr = [], []
+        for idx in xrange(len(eod_dates) - 1, window_size, -1):
+            dates_for_corr.append(eod_dates[idx])
+            hist_corr.append(np.corrcoef(return_s1[idx - window_size:idx] * decay_factors,
+                                         return_s2[idx - window_size:idx] * decay_factors)[0, 1])
+        median_corr = np.percentile(hist_corr, 50)
+        print median_corr, hist_corr
 
     def compare_dates_and_clean(self):
         d1 = self.fetch_close_spots(self.symbol1)
@@ -29,6 +44,13 @@ class CalculateHistoricalCorrelation:
             result[row.date_eod.__str__()] = row.close
         return result
 
+    @staticmethod
+    def get_entropy():
+        if decay_lambda == 1.0:
+            return np.asarray([1.0] * window_size)
+        else:
+            return np.power(decay_lambda, np.linspace(window_size - 1, 0, window_size, endpoint=True))
 
-# corr = CalculateHistoricalCorrelation('FFIV', 'YHOO')
-# corr.calculate_return_and_entropy()
+
+corr = CalculateHistoricalCorrelation('FFIV', 'YHOO')
+corr.calculate_return_and_entropy()
